c3e07b70ac271fd29ae61631404475f47e6e2bde
Yana2022 <94139642+Yana2022@users.noreply.github.com>
2022-07-25 12:03:43 +0000

    Deploying to gh-pages from @ Yana2022/team23@e300712ba9313a90d8f705f693fcd9447cf1aaf3 🚀

---
+++ index.0376c96a.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(e,n){return'<span class="'+n+'">'+(e+1)+"</span>"}}});
+//# sourceMappingURL=index.0376c96a.js.map
