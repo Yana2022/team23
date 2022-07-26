c30f0a67f1913b26aca42621926d7e3e83a2d726
Nikita Sokolov UA <nsokolov0309@gmail.com>
2022-07-26 15:00:44 +0300

    Squashed commit of the following:
    commit eec120871b99e4d344c118fce7aa1d90db3bcdd5
    Merge: 28a4245 e403817
    Author: YanaS <94139642+Yana2022@users.noreply.github.com>
    Date:   Tue Jul 26 11:42:13 2022 +0200
    
        Merge pull request #38 from Yana2022/button-up/changes
    
        button-up
    
    commit e403817e2917c962f4ec8651e9222e89e568f860
    Author: Nikita Sokolov UA <nsokolov0309@gmail.com>
    Date:   Tue Jul 26 12:36:56 2022 +0300
    
        button-up
    
        изменил js
        добавил opacity, теперь кнопка анимированая по кубику.

--- src/js/button-up.js
+++ src/js/button-up.js
@@ -1,18 +1,13 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = () => changeHeaderBackground();
 
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("button-up").style.display = "flex";
-    } else {
-        document.getElementById("button-up").style.display = "none";
-    }
+function changeHeaderBackground() {
+  const header = document.querySelector('[data-header]');
+  const headerOffsetTrigger = header.offsetHeight;
+  const pageOffset = window.pageYOffset;
+
+  if (pageOffset > headerOffsetTrigger) {
+    header.classList.add('no-transparency');
+  } else {
+    header.classList.remove('no-transparency');
+  }
 }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-
