0d59b525acf5d5cda42768b5ab159ce70617f2ea
Yana2022 <94139642+Yana2022@users.noreply.github.com>
2022-07-26 09:42:39 +0000

    Deploying to gh-pages from @ Yana2022/team23@eec120871b99e4d344c118fce7aa1d90db3bcdd5 🚀

--- index.b909ae2d.js
+++
@@ -1,2 +0,0 @@
-function scrollFunction(){document.body.scrollTop>100||document.documentElement.scrollTop>100?document.getElementById("button-up").style.display="flex":document.getElementById("button-up").style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()};
-//# sourceMappingURL=index.b909ae2d.js.map
