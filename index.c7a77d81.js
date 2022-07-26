1e879f46f68bcfdcb83d149a3b41a12f91751171
Yana2022 <94139642+Yana2022@users.noreply.github.com>
2022-07-26 20:11:42 +0000

    Deploying to gh-pages from @ Yana2022/team23@fb26062ec15f9e49845d2b45b99ef52d640a1eed 🚀

---
+++ index.c7a77d81.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-open-popup]"),closeModalBtn:document.querySelector("[data-close-popup]"),modal:document.querySelector("[data-modal-popup]")};function o(){e.modal.classList.toggle("is-hidden-popup")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.c7a77d81.js.map
