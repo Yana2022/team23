ee67ac6bf44ec651959799b005e9aa5b2c645ffe
Yana2022 <94139642+Yana2022@users.noreply.github.com>
2022-07-26 15:14:22 +0000

    Deploying to gh-pages from @ Yana2022/team23@a15a2cb4e076ae87827b192f2fdefb6217047d9a 🚀

--- index.73a4d5c4.js
+++
@@ -1,2 +0,0 @@
-function changeHeaderBackground(){var e=document.querySelector("[data-header]"),n=e.offsetHeight;window.pageYOffset>n?e.classList.add("no-transparency"):e.classList.remove("no-transparency")}window.onscroll=function(){return changeHeaderBackground()};
-//# sourceMappingURL=index.73a4d5c4.js.map
---
+++ index.c541efbd.js
@@ -0,0 +1,2 @@
+function changeHeaderBackground(){var n=document.querySelector("[data-header]"),e=n.offsetHeight;window.pageYOffset>e?n.classList.add("no-transparency"):n.classList.remove("no-transparency")}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){return changeHeaderBackground()};
+//# sourceMappingURL=index.c541efbd.js.map
