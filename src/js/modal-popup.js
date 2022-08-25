4bed7199cd11de71499f1da2b56128984bb96d7d
Nikita Sokolov UA <nsokolov0309@gmail.com>
2022-08-25 14:28:17 +0300

    some-changes on js
    add event
    on esc close modal
    on click background close modal

--- src/js/modal-popup.js
+++ src/js/modal-popup.js
@@ -1,14 +1,39 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-open-popup]"),
+    openModalBtn: document.querySelectorAll("[data-open-popup]"),
     closeModalBtn: document.querySelector("[data-close-popup]"),
     modal: document.querySelector("[data-modal-popup]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn[0].addEventListener("click", openModal);
+   refs.openModalBtn[1].addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+  refs.modal.addEventListener('click', onClickOnBackdropCloseModal);
+ 
 
-  function toggleModal() {
+
+  function openModal() {
     refs.modal.classList.toggle("is-hidden-popup");
+    window.addEventListener('keydown', onPushEscCloseModal)
   }
+
+  function closeModal() {
+    refs.modal.classList.toggle("is-hidden-popup");
+    window.removeEventListener('keydown', onPushEscCloseModal)
+  }
+
+  function onPushEscCloseModal(e) {
+    const ESC_CODE = 'Escape'
+
+    if (e.code === ESC_CODE) {
+      closeModal()
+    }
+  }
+
+  function onClickOnBackdropCloseModal(e) {    
+    if (e.target === e.currentTarget) {
+      closeModal()
+    }
+  }
+
 })();
