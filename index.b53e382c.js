b8125a2b04de6d41260b288c3a834599094cd02b
Yana2022 <94139642+Yana2022@users.noreply.github.com>
2022-08-26 13:29:59 +0000

    Deploying to gh-pages from @ Yana2022/team23@eb26fcf39811f95f4ab90ae4685c1cb510f333a1 🚀

---
+++ index.b53e382c.js
@@ -0,0 +1,2 @@
+new SimpleLightbox(".gallery a").options.captions=!1;
+//# sourceMappingURL=index.b53e382c.js.map
---
+++ index.ea7227a5.js
@@ -0,0 +1,2 @@
+new SimpleLightbox(".gallery a").options.captions=!1;
+//# sourceMappingURL=index.ea7227a5.js.map
